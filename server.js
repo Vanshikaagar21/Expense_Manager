5e69cbab95b6d907b68528f5bf82495e56c52226
Vanshika <vanshika.agar@gmail.com>
2023-10-14 13:28:27 +0530

    Basic server and database connection setup

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDB = require("./config/connectDB");
+
+//config dot env file
+dotenv.config();
+
+//database call
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+//Port
+const PORT = 8080 || process.env.PORT;
+
+//listen server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
