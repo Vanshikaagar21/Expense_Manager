7b3719fcf3f1ce61e7c1f736bfff1b456703b7e4
Vanshika <vanshika.agar@gmail.com>
2023-10-16 00:51:42 +0530

    add turnover analytics

--- client/src/components/Analytics.jsx
+++ client/src/components/Analytics.jsx
@@ -15,6 +15,24 @@ const Analytics = ({ allTransaction }) => {
   const totalExpensePercent =
     (totalExpenseTransactions.length / totalTransaction) * 100;
 
+  //total Turnover
+  const totalTurnover = allTransaction.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0
+  );
+  const totalIncomeTurnover = allTransaction
+    .filter((transaction) => transaction.type === "income")
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const totalExpenseTurnover = allTransaction
+    .filter((transaction) => transaction.type === "expense")
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const totalIncomeTurnoverPercent =
+    (totalIncomeTurnover / totalTurnover) * 100;
+  const totalExpenseTurnoverPercent =
+    (totalExpenseTurnover / totalTurnover) * 100;
+
   return (
     <>
       <div className="row m-3">
@@ -42,6 +60,29 @@ const Analytics = ({ allTransaction }) => {
                   strokeColor={"red"}
                   className="mx-2"
                   percent={totalExpensePercent.toFixed(0)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card">
+            <div className="card-header">Total Turnover : {totalTurnover}</div>
+            <div className="card-body">
+              <h5 className="text-success">Income : {totalIncomeTurnover}</h5>
+              <h5 className="text-danger">Expense : {totalExpenseTurnover}</h5>
+              <div>
+                <Progress
+                  type="circle"
+                  strokeColor={"green"}
+                  className="mx-2"
+                  percent={totalIncomeTurnoverPercent.toFixed(0)}
+                />
+                <Progress
+                  type="circle"
+                  strokeColor={"red"}
+                  className="mx-2"
+                  percent={totalExpenseTurnoverPercent.toFixed(0)}
                 />
               </div>
             </div>
