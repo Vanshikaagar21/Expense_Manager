ec883c6b5b7a1e77068d25b7de7ffaf92b65cce6
Vanshika <vanshika.agar@gmail.com>
2023-10-15 22:17:09 +0530

    attach add transaction functionality

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import { message, Form, Input, Modal, Select } from "antd";
 import Layout from "../components/Layout/Layout";
+import axios from "axios";
+import Spinner from "./../components/Spinner";
 
 const HomePage = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  //form handling
+  const handleSubmit = async (values) => {
+    try {
+      const user = JSON.parse(localStorage.getItem("user"));
+      setLoading(true);
+      await axios.post(
+        "http://localhost:8080/api/v1/transactions/add-transaction",
+        {
+          ...values,
+          userid: user._id,
+        }
+      );
+      setLoading(false);
+      message.success("Transaction Added Successfully");
+      setShowModal(false);
+    } catch (error) {
+      setLoading(false);
+      message.error("Failed to add transaction");
+    }
+  };
   return (
     <Layout>
-      <h1>Homepage</h1>
+      {loading && <Spinner />}
+      <div className="filters">
+        <div>range filters</div>
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => setShowModal(true)}
+          >
+            Add new
+          </button>
+        </div>
+      </div>
+      <div className="content"></div>
+      <Modal
+        title="Add Transaction"
+        open={showModal}
+        onCancel={() => setShowModal(false)}
+        footer={false}
+      >
+        <Form layout="vertical" onFinish={handleSubmit}>
+          <Form.Item label="Amount" name="amount">
+            <Input type="number" />
+          </Form.Item>
+          <Form.Item label="Type" name="type">
+            <Select>
+              <Select.Option value="income">Income</Select.Option>
+              <Select.Option value="expense">Expense</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Category" name="category">
+            <Select>
+              <Select.Option value="salary">Salary</Select.Option>
+              <Select.Option value="gifts">Gifts</Select.Option>
+              <Select.Option value="wallet">Wallet</Select.Option>
+              <Select.Option value="general">General</Select.Option>
+              <Select.Option value="eating">Eating Out</Select.Option>
+              <Select.Option value="shopping">Shopping</Select.Option>
+              <Select.Option value="entertainment">Entertainment</Select.Option>
+              <Select.Option value="fuel">Fuel</Select.Option>
+              <Select.Option value="travel">Travel</Select.Option>
+              <Select.Option value="medical">Medical</Select.Option>
+              <Select.Option value="fee">Fee</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Date" name="date">
+            <Input type="date" />
+          </Form.Item>
+          <Form.Item label="Description" name="description">
+            <Input type="text" />
+          </Form.Item>
+          <div className="d-flex justify-content-end">
+            <button type="submit" className="btn btn-primary">
+              SAVE
+            </button>
+          </div>
+        </Form>
+      </Modal>
     </Layout>
   );
   s;
--- models/transactionModel.js
+++ models/transactionModel.js
@@ -2,16 +2,21 @@ const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema(
   {
+    userid: {
+      type: String,
+      required: [true, "UserId is required"],
+    },
     amount: {
       type: Number,
       required: [true, "Amount is required"],
     },
+    type: {
+      type: String,
+      required: [true, "Type is required"],
+    },
     category: {
       type: String,
       required: [true, "Category is required"],
-    },
-    refrence: {
-      type: String,
     },
     description: {
       type: String,
